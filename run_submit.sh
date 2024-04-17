b24491c93a815cec52e8c339b83346b0469d2e09
Wanggcong <1311328021@qq.com>
2024-04-17 12:20:20 +0800

    2024-04-17-12:20:19

--- run_submit.sh
+++ run_submit.sh
@@ -1,9 +1,9 @@
 current_time=$(date +"%Y-%m-%d-%T")
 echo "Current time: $current_time"
 # git remote add homepage https://github.com/Wanggcong/Wanggcong.github.io.git
-# check local branch via "git branch"
-# check local branch by check the repo of Github
-git pull homepage master:master # local branch: remote branch
+# check local branch via "git branch", here is "master"
+# check local branch by check the repo of Github, here is "master"
+git pull homepage master:master # git pull <remote> <remote_branch>:<local_branch>
 git add .
 git commit -m "$current_time"
-git push -u homepage master:master
+git push -u homepage master:master # git push -u <remote> <local_branch>:<remote_branch>
